1a51705aafbe43cd82a4908bd88c19f4d99284ff
sawantpalvi1234 <sawantpalvi087@gmail>
2020-08-20 03:34:13 +0530

    Added:Functionality to check luckiest and unluckiest day

--- Gambler.sh
+++ Gambler.sh
@@ -22,7 +22,7 @@ day(){
         done
 	actualGain=$(($STAKE-100))
 }
-
+a=0
 total(){
 for ((i=1;i<=30;i++))
 do
@@ -37,7 +37,34 @@ do
 		loseDay=$(($loseDay + 1))
 	fi
 	totalAmount=$(($totalAmount+$actualGain))
+	daydict[$i]=$totalAmount
+	arr[$a]=$totalAmount
+	a=$(($a + 1))
 done
+for ((j=0;j<$(($a-1));j++))
+do
+        for((k=j+1;k<$(($a-1));k++))
+        do
+                if((arr[j] > arr[k]))
+                then
+                        temp=${arr[j]}
+                        arr[j]=${arr[k]}
+                        arr[k]=$temp
+                fi
+        done
+done
+for ((i=1;i<=30;i++))
+do
+	if ((daydict[i] == arr[$a-1]))
+	then
+		echo "Luckiest day of gambler was Day $i"
+	fi
+	if ((daydict[i] == arr[0]))
+        then
+                echo "Unluckiest day of gambler was Day $i"
+        fi
+done
+
 echo "Total number of days won in month is $winDay and amount gained is $WON"
 echo "Total number of days loose in month is $loseDay and amount loss is $LOSE"
 if [ $totalAmount -gt 0 ]
