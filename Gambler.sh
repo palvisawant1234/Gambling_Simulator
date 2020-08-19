2fd36118cd792b32af86e835f1fc88a809b538f1
sawantpalvi1234 <sawantpalvi087@gmail>
2020-08-20 00:48:36 +0530

    Added:Fuctinality to check Win or Loose upto DailyLimit

--- Gambler.sh
+++ Gambler.sh
@@ -3,15 +3,27 @@
 DAY=0
 STAKE=100
 BET=1
+MAXLIMIT=$(($STAKE + $(($STAKE * 50 / 100)) ))
+MINLIMIT=$(($STAKE - $(($STAKE * 50 / 100)) ))
+
 bet(){
-	if [ $((RANDOM % 2)) -eq 1 ]
-	then
-		echo "You Won"
-		STAKE=$(($STAKE + $BET))
-	else
-		echo "You Loose"
-		STAKE=$(($STAKE - $BET))
-	fi
+        if [ $((RANDOM % 2)) -eq 1 ]
+        then
+                echo "You Won"
+                STAKE=$(($STAKE + $BET))
+        else
+                echo "You Loose"
+                STAKE=$(($STAKE - $BET))
+        fi
 }
-bet
+
+day(){
+        STAKE=100
+        while [ $STAKE -lt $MAXLIMIT ] && [ $STAKE -gt $MINLIMIT ]
+        do
+                bet
+        done
+        echo "Your final stake = $STAKE"
+}
+day
 
