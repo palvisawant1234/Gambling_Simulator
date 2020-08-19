01611c744108b009fb47c3b771f8fffb7b106a86
sawantpalvi1234 <sawantpalvi087@gmail>
2020-08-20 00:31:44 +0530

    Added:Fuctionality to check Win or Loose

--- Gambler.sh
+++ Gambler.sh
@@ -3,3 +3,15 @@
 DAY=0
 STAKE=100
 BET=1
+bet(){
+	if [ $((RANDOM % 2)) -eq 1 ]
+	then
+		echo "You Won"
+		STAKE=$(($STAKE + $BET))
+	else
+		echo "You Loose"
+		STAKE=$(($STAKE - $BET))
+	fi
+}
+bet
+
